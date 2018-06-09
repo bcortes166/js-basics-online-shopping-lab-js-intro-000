7e9d7e9378bd3765bb66659d8d4027adc4218e92
Brian Cortes <briancorts166@gmail.com>
2018-06-09 19:03:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,21 +54,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  var obj; var key; var oldCart = cart; var itemName; var name; var i = 0
-
+  var i = 0;
   while (i < cart.length) {
-    obj = cart[i]
-    name = obj[itemName]
-    if (name === item) {
+    if (cart[i]['itemName'] === item) {
       cart.splice(i, 1)
-      --i
+    } else {
+      i++
     }
-    i++
-  }
-  if (oldCart === cart) {
-    return 'That item is not in your cart.'
-  } else {
-    return cart
   }
 }
 
